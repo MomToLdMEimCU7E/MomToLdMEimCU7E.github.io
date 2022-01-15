4b1154be1863d0d7577ddea350893ae20239bc42
MomToLdMEimCU7E <1453542087@qq.com>
2022-01-15 09:30:47 +0800

    ajax1

--- js/main.js
+++ js/main.js
@@ -19,8 +19,8 @@ $.getJSON("../data/posts.json","",function(result){
     str+='<i class="far fa-clock"></i>';
     str+='<span>'+postobj.createdtime+'</span>';
     str+='</div>';
-    str+='<h3 class="title">'+postobj.title+'</h3>';
-    str+='<p class="text">'+postobj.content+'</p>';
+    str+='<h3 class="title">'+postobj.title_s+'</h3>';
+    str+='<p class="text">'+postobj.content_s+'</p>';
     str+='<div class="links"><a href="#" class="user"><i class="far fa-user"></i><span>by 夏梦</span></a><a href="edit.html" class="icon"><i class="iconfont icon-xiugai"></i><span>修改</span></a><a href="delete.html" class="icon"><i class="iconfont icon-shanchu"></i><span>删除</span></a></div></div>';
   }
   $("#posts_content").html(str);
--- js/new.js
+++ js/new.js
@@ -1,20 +1,16 @@
-$("#submit").click(function(){
-    var formObject = {};
-    var formArray = $("#postInfo").serializeArray();
-    $.each(formArray,function(i,item){
-        formObject[item.name]=item.value;
-    });
+function newText(){
     $.ajax({
-        type:"get",
+        type:"POST",
         url:"../data/posts.json",
         dataType:'json',
         contentType: "application/json; charset=utf-8",
-        data: JSON.stringify(formObject),
-        success: function(data){
+        data: $('#postInfo').serialize(),
+        success: function(result){
             alert("提交成功");
+            console.log(result);
         },
         error: function(){
             alert("提交失败");
         }
     })
-})
+}
